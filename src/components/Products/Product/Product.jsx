1c650a317f58443431a3f90a48d07718a091fe76
Fahmidur-arnob <arnobdev93@gmail.com>
2023-04-17 11:56:59 +0600

    Done adding products and animations, now just making some little changes in the styling

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,6 +1,24 @@
-import "./Product.scss";
-const Product = () => {
-    return <div>Product</div>;
-};
 
+import prod from '../../../assets/products/earbuds-prod-1.webp';
+
+import "./Product.scss";
+
+const Product = () => {
+    return (
+        <div className="product-card">
+            <div className="thumbnail">
+                <img src={prod} alt="earbuds"/>
+            </div>
+            <div className="product-details">
+                <span className="name">
+                    Product Name
+                </span>
+                <span className="price">
+                    &#36;499
+                </span>
+            </div>
+        </div>
+    );
+};
+//2:06:03
 export default Product;
