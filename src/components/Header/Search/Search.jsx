d88b030c033418d58e2bd33b2c212b2dfdbb31aa
Fahmidur-arnob <arnobdev93@gmail.com>
2023-04-19 20:29:26 +0600

    done with the search option and making cartItem

--- src/components/Header/Search/Search.jsx
+++ src/components/Header/Search/Search.jsx
@@ -36,7 +36,13 @@ const Search = ({ setShowSearch }) => {
             <div className="search-result-content">
                 <div className="search-results">
                     {data?.data?.map(item => (
-                        <div key={item.id} className="search-result-item">
+                        <div 
+                            key={item.id} 
+                            className="search-result-item"
+                            onClick={() => {
+                                navigate("/product/" + item.id); 
+                            }}
+                        >
                             <div className="img-container">
                                 <img 
                                     src={process.env.REACT_APP_DEV_URL + item.attributes.img.data[0].attributes.url} 
@@ -58,5 +64,7 @@ const Search = ({ setShowSearch }) => {
         </div>
     );
 };
+
+//5:58:43
 
 export default Search;
